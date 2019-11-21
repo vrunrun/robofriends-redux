609c198990dbdc294c504b3fef9a16f33a6eb16d
kl <kl@debian>
2019-11-21 22:18:48 +0800

    Add tests for container component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import CardList from "../components/CardList";
-import SearchBox from "../components/SearchBox";
-import Scroll from "../components/Scroll";
-import Header from "../components/Header";
 import "./App.css";
-
 import { setSearchField, requestRobots } from "../actions";
+import MainPage from "../components/MainPage";
 
 const mapStateToProps = state => {
   return {
@@ -25,26 +21,8 @@ const mapDispatchToProps = dispatch => {
 };
 
 class App extends Component {
-  componentDidMount() {
-    this.props.onRequestRobots();
-  }
-
   render() {
-    const { searchField, onSearchChange, robots, isPending } = this.props;
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    });
-    return isPending ? (
-      <h1>Loading</h1>
-    ) : (
-      <div className="tc">
-        <Header />
-        <SearchBox searchChange={onSearchChange} />
-        <Scroll>
-          <CardList robots={filteredRobots} />
-        </Scroll>
-      </div>
-    );
+    return <MainPage {...this.props} />;
   }
 }
 
